d83d7752d14fecf5f1126deb9c33d989a145368a
Joshua Westerheide <dev@jdoubleu.de>
2017-11-21 10:54:11 +0100

    Set extension version

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,6 +10,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Simple 404 handler',
     'description' => 'Enables simple 404 Page NotFound handling, also for multilingual sites.',
+    'version' => '1.0.0',
     'category' => 'fe',
     'constraints' => [
         'depends' => [
